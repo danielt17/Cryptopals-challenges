a6ed1e21a1b2fced29fe43622fab5c8fa8491e9a
danielt17 <60748408+danielt17@users.noreply.github.com>
2021-09-23 15:33:27 +0300

    Add files via upload

---
+++ Q17.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 14:59:23 2021
+
+@author: danie
+"""
+
+
+# %% Challange
+
+# https://cryptopals.com/sets/1/challenges/7
+
+# %% Imports
+
+from base64 import b64decode
+from Crypto.Cipher import AES
+
+# %% Functions
+
+def aes_ecb_decrypt(cypherText,Key):
+    AES_ECB_cypher = AES.new(Key,AES.MODE_ECB)
+    plaintext = AES_ECB_cypher.decrypt(cypherText)
+    return plaintext
+
+# %% Main
+
+if __name__ == '__main__':
+    with open('17.txt') as f:
+        cypherText = b64decode(f.read())
+    Key = "YELLOW SUBMARINE"
+    plaintext = aes_ecb_decrypt(cypherText,Key).decode()
+    print(plaintext)
